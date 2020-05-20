07f80d4fa301e4dce4bd8751e5d06922be42b78a
ol222hf <ol222hf@student.lnu.se>
2020-05-20 22:51:10 +0200

    dynamically sets name and random limit for each player

--- twentyone/Game.cs
+++ twentyone/Game.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace examination_3
@@ -19,7 +20,10 @@ namespace examination_3
             List<Player> players = new List<Player>();
             for (int i = 0; i < NumberOfPlayers; i++)
             {
-                Player player = new Player("Player" + (i + 1), 15);
+                string name = $"Player{i + 1}";
+                int limit = new Random().Next(15, 20);
+
+                Player player = new Player(name, limit);
                 players.Add(player);
             }
             Players = players;
