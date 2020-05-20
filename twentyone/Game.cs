0b2bfd196e34d229e31cb3826e3d6cd2c1bce5ee
ol222hf <ol222hf@student.lnu.se>
2020-05-20 22:45:14 +0200

    generates players based on numberOfPlayers param

--- twentyone/Game.cs
+++ twentyone/Game.cs
@@ -1,14 +1,28 @@
+using System.Collections.Generic;
+
 namespace examination_3
 {
     class Game
     {
         public Deck Deck { get; private set; }
         public int NumberOfPlayers { get; private set; }
+        public List<Player> Players { get; private set; }
 
         public Game(int numberOfPlayers)
         {
             NumberOfPlayers = numberOfPlayers;
             Deck = new Deck();
+        }
+
+        public void generatePlayers()
+        {
+            List<Player> players = new List<Player>();
+            for (int i = 0; i < NumberOfPlayers; i++)
+            {
+                Player player = new Player("Player" + (i + 1), 15);
+                players.Add(player);
+            }
+            Players = players;
         }
 
         public void StartGame()
