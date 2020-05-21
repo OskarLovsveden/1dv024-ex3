46cd862dcaee52a3c77f283b8143388d47d95a6f
ol222hf <ol222hf@student.lnu.se>
2020-05-21 19:47:35 +0200

    Adding constructor

--- twentyone/card/Card.cs
+++ twentyone/card/Card.cs
@@ -2,8 +2,15 @@ namespace examination_3
 {
     class Card
     {
-        public CardSuit Suit { get; set; }
-        public CardRank Rank { get; set; }
-        public int Value { get; set; }
+        public CardSuit Suit { get; private set; }
+        public CardRank Rank { get; private set; }
+        public int Value { get; private set; }
+
+        public Card(CardSuit suit, CardRank rank, int value)
+        {
+            Suit = suit;
+            Rank = rank;
+            Value = value;
+        }
     }
 }
