4b8c1d7ac71d0641868d5b1a1ceafe1e381156fd
ol222hf <ol222hf@student.lnu.se>
2020-05-21 11:18:59 +0200

    Deck no longer knows about cards rank/suit, only generates 52 cards

--- twentyone/Deck.cs
+++ twentyone/Deck.cs
@@ -5,34 +5,98 @@ namespace examination_3
 {
     class Deck
     {
-        public static List<Card> GenerateDeck()
+        public List<Card> Cards { get; set; }
+
+        public Deck()
         {
-            string[] suits = { "Hearts", "Spades", "Clubs", "Diamonds" };
-            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
-            int[] values = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            Cards = new List<Card>();
 
-            List<Card> deck = new List<Card>();
+            int[] value = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            int index = 0;
 
-            foreach (var suit in suits)
+            foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
             {
-                for (int i = 0; i < values.Length; i++)
+                foreach (CardRank rank in Enum.GetValues(typeof(CardRank)))
                 {
-                    deck.Add(new Card(values[i], (ranks[i] + " of " + suit)));
+                    switch (rank)
+                    {
+                        case CardRank.Jack:
+                            Cards.Add(new Card()
+                            {
+                                Suit = suit,
+                                Rank = rank,
+                                Value = 11
+                            });
+                            break;
+                        case CardRank.Queen:
+                            Cards.Add(new Card()
+                            {
+                                Suit = suit,
+                                Rank = rank,
+                                Value = 12
+                            });
+                            break;
+                        case CardRank.King:
+                            Cards.Add(new Card()
+                            {
+                                Suit = suit,
+                                Rank = rank,
+                                Value = 13
+                            });
+                            break;
+                        case CardRank.Ace:
+                            Cards.Add(new Card()
+                            {
+                                Suit = suit,
+                                Rank = rank,
+                                Value = 14
+                            });
+                            break;
+                        default:
+                            Cards.Add(new Card()
+                            {
+                                Suit = suit,
+                                Rank = rank,
+                                Value = value[index]
+                            });
+                            break;
+                    }
+                    index++;
                 }
             }
-
-            return deck;
         }
+        // public static Card[] GenerateDeck()
+        // {
+        //     // var cardValue = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
 
-        public static void ShuffleDeck(List<Card> deck)
+        //     Card[] deck = new Card[52];
+
+        //     int index = 0;
+
+        //     foreach (var suit in suits)
+        //     {
+        //         for (int i = 0; i < values.Length; i++)
+        //         {
+        //             (new Card(values[i], (ranks[i] + " of " + suit)));
+        //             index++;
+        //         }
+        //     }
+
+        //     return deck;
+        // }
+
+        public void Shuffle()
         {
-            int n = deck.ToArray().Length;
-            for (int i = 0; i < (n - 1); i++)
+            Random r = new Random();
+
+            List<Card> cards = Cards;
+
+            for (int n = cards.Count - 1; n > 0; --n)
             {
-                int r = i + new Random().Next(n - i);
-                Card t = deck[r];
-                deck[r] = deck[i];
-                deck[i] = t;
+                int k = r.Next(n + 1);
+                Card temp = cards[n];
+                cards[n] = cards[k];
+                cards[k] = temp;
             }
         }
     }
