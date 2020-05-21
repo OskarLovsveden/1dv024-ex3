18344e3a79cccead4cd8c88f0dba401dd1767d7b
ol222hf <ol222hf@student.lnu.se>
2020-05-21 19:52:23 +0200

    Adding method to deal one card to each player

--- twentyone/Game.cs
+++ twentyone/Game.cs
@@ -5,16 +5,21 @@ namespace examination_3
 {
     class Game
     {
-        public List<Card> Deck21 { get; private set; }
-        public int NumberOfPlayers { get; private set; }
-        public List<Player> Players { get; private set; }
+        public Deck DrawPile { get; set; }
+        public List<Card> ThrowPile { get; set; }
+        public int NumberOfPlayers { get; set; }
+        public List<Player> Players { get; set; }
 
         public Game(int numberOfPlayers)
         {
             NumberOfPlayers = numberOfPlayers;
+            DrawPile = new Deck();
+            DrawPile.Shuffle();
+            ThrowPile = new List<Card>();
+
         }
 
-        public void generatePlayers()
+        public void GeneratePlayers()
         {
             List<Player> players = new List<Player>();
             for (int i = 0; i < NumberOfPlayers; i++)
@@ -28,11 +33,23 @@ namespace examination_3
             Players = players;
         }
 
+        public void DealOneCardEach()
+        {
+            foreach (Player player in Players)
+            {
+                Card drawnCard = DrawPile.Draw();
+                player.Cards.Add(drawnCard);
+            }
+        }
+
         public void StartGame()
         {
-            List<Card> deck = Deck.GenerateDeck();
-            Deck.ShuffleDeck(deck);
-            Deck21 = deck;
+            GeneratePlayers();
+            DealOneCardEach();
+            // Console.WriteLine($"{drawnCard.Rank} of {drawnCard.Suit} with value: {drawnCard.Value}");
+            // ThrowPile.Add(drawnCard);
+            // Console.WriteLine(DrawPile.Cards.ToArray().Length);
+            // Console.WriteLine(ThrowPile.ToArray().Length);
         }
     }
 }
