1811ab2bd6e97362c9bfce8ea65e3b861b48e1ba
ol222hf <ol222hf@student.lnu.se>
2020-05-21 11:18:17 +0200

    Moving and rewriting Card class

---
+++ twentyone/card/Card.cs
@@ -0,0 +1,9 @@
+namespace examination_3
+{
+    class Card
+    {
+        public CardSuit Suit { get; set; }
+        public CardRank Rank { get; set; }
+        public int Value { get; set; }
+    }
+}
