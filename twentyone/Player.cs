95ae3b344a264524a2ed655717372cc0562da7fc
ol222hf <ol222hf@student.lnu.se>
2020-05-22 11:47:24 +0200

    adding private to setters

--- twentyone/Player.cs
+++ twentyone/Player.cs
@@ -4,8 +4,8 @@ namespace examination_3
 {
     class Player : Hand
     {
-        public string Name { get; set; }
-        public int Limit { get; set; }
+        public string Name { get; private set; }
+        public int Limit { get; private set; }
 
         public Player(string name, int limit)
         : base()
@@ -16,17 +16,16 @@ namespace examination_3
 
         public override string ToString()
         {
-            string str = "";
-            int sum = SumOfHand();
+            string cardsInHand = "";
 
             foreach (Card card in Cards)
             {
-                str += $"[{card.Rank} of {card.Suit}] ";
+                cardsInHand += $"[{card.Rank} of {card.Suit}] ";
             }
 
-            return sum <= 21 ?
-                String.Format("{0,-12}: Sum: ({1}) {2}", Name, sum, str) :
-                String.Format("{0,-12}: Sum: ({1}) {2} BUSTED!", Name, sum, str);
+            return SumOfHand <= 21 ?
+                String.Format("{0,-12}: Sum: ({1}) {2}", Name, SumOfHand, cardsInHand) :
+                String.Format("{0,-12}: Sum: ({1}) {2} BUSTED!", Name, SumOfHand, cardsInHand);
         }
     }
 }
